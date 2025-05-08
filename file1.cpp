860edfbfdf328f9e6ac42fc571ac6cd2256b623f
DhandhaDendriyaE <dandadendriya17@gmail.com>
2025-05-08 09:54:11 +0700

    first commit

---
+++ file1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+
+class node
+{
+    public:
+    int noMhs;
+    node *next;
+};
+
+class list
+{
+    node *START;
+    public:
+}
